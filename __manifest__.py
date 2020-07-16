d64dcfdb042b6f422103441095cf59f11f75a981
James Vibar <vibarjamess@gmail.com>
2020-07-16 19:05:05 +0800

    add acttive field

--- __manifest__.py
+++ __manifest__.py
@@ -20,7 +20,7 @@
     'version': '0',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'mail', 'contacts'],
+    'depends': ['base', 'mail', 'contacts', 'website', 'website_form'],
 
     # always loaded
     'data': [
